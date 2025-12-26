7032e087d3dcb41bfca5bccc1fc9fccd2c1c7fb5
OGEmanuel <1blacksanta416@gmail.com>
2025-12-26 04:55:39 +0100

    add:mobile responsiveness for events details page

--- src/pages/home/events/details/index.tsx
+++ src/pages/home/events/details/index.tsx
@@ -24,15 +24,18 @@ import EmptyState from './empty-state';
 const EventsDetails = () => {
   return (
     <section className="flex min-h-[calc(100vh-326px)] justify-center md:min-h-[calc(100vh-73px)]">
-      <div className="font-inter flex w-full max-w-300 flex-col gap-6 pt-6">
-        <div className="flex flex-col gap-3">
-          <Link
-            to={'/'}
-            className="flex items-center gap-2 leading-[100%] -tracking-[0.02em] text-[#575554]"
-          >
-            <ArrowLeft className="size-5" />
-            Back to events
-          </Link>
+      <div className="font-inter flex w-full max-w-300 flex-col gap-6 max-md:py-5 sm:pt-6">
+        <div className="flex flex-col gap-2 max-md:px-5 md:gap-3 md:max-xl:px-8">
+          <div className="flex items-center justify-between">
+            <Link
+              to={'/'}
+              className="flex items-center gap-2 leading-[100%] -tracking-[0.02em] text-[#575554]"
+            >
+              <ArrowLeft className="size-5" />
+              Back to events
+            </Link>
+            <Menu className="lg:hidden" />
+          </div>
           <GuestDetails />
         </div>
         <InviteStats />
@@ -46,7 +49,7 @@ export default EventsDetails;
 
 const GuestDetails = () => {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex max-lg:flex-col max-lg:gap-6 md:justify-between lg:items-center">
       <div className="flex flex-col gap-2">
         <h1 className="font-serif text-2xl/8 text-[#212121]">
           Mr. & Mrs. Williams' Wedding
@@ -58,13 +61,13 @@ const GuestDetails = () => {
           </p>
         </div>
       </div>
-      <div className="flex items-center gap-2">
-        <AddGuest />
-        <Button>
+      <div className="flex items-center gap-2 md:max-lg:justify-end">
+        <AddGuest className="max-md:flex-1" />
+        <Button className="max-md:flex-1">
           <Send />
           Send invites
         </Button>
-        <Menu />
+        <Menu className="max-lg:hidden" />
       </div>
     </div>
   );
@@ -72,45 +75,49 @@ const GuestDetails = () => {
 
 const InviteStats = () => {
   return (
-    <div className="flex justify-between rounded-[12px] border border-[#0000001A] [&>div]:flex [&>div]:basis-full [&>div]:flex-col [&>div]:gap-3 [&>div]:border-r [&>div]:border-[#0000001A] [&>div]:p-4 [&>div]:last:border-r-0">
-      <div>
-        <p className="leading-6 -tracking-[0.02em] text-[#575554]">
-          Total guest
-        </p>
-        <p className="font-serif text-2xl/8 text-[#212121]">0</p>
-      </div>
-      <div>
-        <p className="leading-6 -tracking-[0.02em] text-[#575554]">
-          Invite sent
-        </p>
-        <p className="font-serif text-2xl/8 text-[#212121]">0</p>
-      </div>
-      <div>
-        <p className="leading-6 -tracking-[0.02em] text-[#575554]">
-          Confirmed RSVP
-        </p>
-        <p className="font-serif text-2xl/8 text-[#212121]">0</p>
-      </div>
-      <div>
-        <p className="leading-6 -tracking-[0.02em] text-[#575554]">
-          Awaiting Response
-        </p>
-        <p className="font-serif text-2xl/8 text-[#212121]">0</p>
-      </div>
-      <div>
-        <p className="leading-6 -tracking-[0.02em] text-[#575554]">
-          Failed Delivery
-        </p>
-        <p className="font-serif text-2xl/8 text-[#212121]">0</p>
+    <div className="scrollbar-hidden max-lg:w-full max-lg:overflow-auto max-md:px-5 md:max-xl:px-8">
+      <div className="flex justify-between rounded-[12px] border border-[#0000001A] max-lg:w-max [&>div]:flex [&>div]:flex-col [&>div]:gap-3 [&>div]:border-r [&>div]:border-[#0000001A] [&>div]:p-4 [&>div]:last:border-r-0 max-lg:[&>div]:w-43 lg:[&>div]:basis-full">
+        <div>
+          <p className="leading-6 -tracking-[0.02em] text-[#575554]">
+            Total guest
+          </p>
+          <p className="font-serif text-2xl/8 text-[#212121]">0</p>
+        </div>
+        <div>
+          <p className="leading-6 -tracking-[0.02em] text-[#575554]">
+            Invite sent
+          </p>
+          <p className="font-serif text-2xl/8 text-[#212121]">0</p>
+        </div>
+        <div>
+          <p className="leading-6 -tracking-[0.02em] text-[#575554]">
+            Confirmed RSVP
+          </p>
+          <p className="font-serif text-2xl/8 text-[#212121]">0</p>
+        </div>
+        <div>
+          <p className="leading-6 -tracking-[0.02em] text-[#575554]">
+            Awaiting Response
+          </p>
+          <p className="font-serif text-2xl/8 text-[#212121]">0</p>
+        </div>
+        <div>
+          <p className="leading-6 -tracking-[0.02em] text-[#575554]">
+            Failed Delivery
+          </p>
+          <p className="font-serif text-2xl/8 text-[#212121]">0</p>
+        </div>
       </div>
     </div>
   );
 };
 
-const AddGuest = () => {
+const AddGuest = (props: { className?: string }) => {
+  const { className } = props;
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild className={className}>
         <Button
           variant={'neutral'}
           className="gap-2 [&_svg:not([class*='size-'])]:size-6"
@@ -140,10 +147,12 @@ const AddGuest = () => {
   );
 };
 
-const Menu = () => {
+const Menu = (props: { className?: string }) => {
+  const { className } = props;
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild className={className}>
         <Button size={'icon-lg'} variant={'neutral'}>
           <MoreVertical className="text-[#575554]" />
         </Button>
