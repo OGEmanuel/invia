fc43ff646f04c0e362bda34f1643f4ba6fe9f28c
OGEmanuel <1blacksanta416@gmail.com>
2025-12-29 02:07:50 +0100

    add:mobile responsiveness for guest list view

--- src/pages/home/events/details/guest-list.tsx
+++ src/pages/home/events/details/guest-list.tsx
@@ -13,6 +13,7 @@ import { columns } from './columns';
 import type { Guests } from '@/lib/constants';
 import { Pagination } from '@/components/ui/custom/pagination';
 import { useState } from 'react';
+import { cn } from '@/lib/utils';
 
 const GuestList = () => {
   const [page, setPage] = useState(1);
@@ -34,10 +35,21 @@ const GuestList = () => {
   }
 
   return (
-    <section className="flex flex-col gap-6">
+    <section className="flex flex-col gap-6 max-md:px-5 md:max-xl:px-8">
       <GuestListConfig />
+      <hr className="border-y-[0.5px] border-[#00000014] sm:hidden" />
+      <div className="flex flex-col gap-6 xl:hidden">
+        {data.map((guest, i) => (
+          <div key={guest.id} className="flex flex-col gap-6">
+            <GuestCard {...guest} />
+            {i !== data.length - 1 && (
+              <hr className="h-2.5 border-none bg-[#F7F5F2]" />
+            )}
+          </div>
+        ))}
+      </div>
       <div className="flex flex-col gap-2">
-        <DataTable data={data} columns={columns} />
+        <DataTable data={data} columns={columns} className="max-xl:hidden" />
         <Pagination currentPage={page} totalPages={10} onPageChange={setPage} />
       </div>
     </section>
@@ -52,7 +64,7 @@ const GuestListConfig = () => {
   const [___, setFilterByStatus] = useQueryState('status');
 
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 border-[#00000014]">
       <div className="flex items-center justify-between">
         <h2 className="leading-[100%] font-medium -tracking-[0.02em] text-[#212121]">
           Guest List
@@ -61,7 +73,7 @@ const GuestListConfig = () => {
           Showing 129 of 1,224
         </p>
       </div>
-      <div className="flex items-center justify-between">
+      <div className="flex justify-between max-sm:flex-col max-sm:gap-4 sm:items-center">
         <div className="relative w-full max-w-70">
           <Input
             name="search"
@@ -71,7 +83,7 @@ const GuestListConfig = () => {
           />
           <Search className="absolute top-1/2 size-5 -translate-y-1/2 text-[#575554]" />
         </div>
-        <div className="flex items-center gap-6 [&>div]:flex [&>div]:items-center [&>div]:gap-1 [&>div>p]:leading-[100%] [&>div>p]:-tracking-[0.02em] [&>div>p]:text-[#575554]">
+        <div className="flex items-center gap-6 [&>div]:flex [&>div]:items-center [&>div]:gap-1 max-sm:[&>div]:flex-wrap [&>div>p]:leading-[100%] [&>div>p]:-tracking-[0.02em] [&>div>p]:text-[#575554]">
           <div>
             <p>Party:</p>
             <FilterGuestList
@@ -111,6 +123,90 @@ const GuestListConfig = () => {
               ]}
             />
           </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const GuestCard = ({ guest, party, contact, status, rsvp }: Guests) => {
+  const statusTextColor =
+    status === 'sent'
+      ? 'text-[#0088FF]'
+      : status === 'pending'
+        ? 'text-[#FF8D28]'
+        : status === 'delivered'
+          ? 'text-[#2B8309]'
+          : 'text-[#6155F5]';
+
+  const statusBgColor =
+    status === 'sent'
+      ? 'bg-[#0088FF1A]'
+      : status === 'pending'
+        ? 'bg-[#FF8D281A]'
+        : status === 'delivered'
+          ? 'bg-[#3FC70A1A]'
+          : 'bg-[#6155F51A]';
+
+  const statusBorderColor =
+    status === 'sent'
+      ? 'border-[#0088FF33]'
+      : status === 'pending'
+        ? 'border-[#FF8D2833]'
+        : status === 'delivered'
+          ? 'border-[#3FC70A33]'
+          : 'border-[#6155F533]';
+
+  const rsvpTextColor =
+    rsvp === 'awaiting' ? 'text-[#FF8D28]' : 'text-[#2B8309]';
+
+  const rsvpBgColor = rsvp === 'awaiting' ? 'bg-[#FF8D281A]' : 'bg-[#3FC70A1A]';
+
+  const rsvpBorderColor =
+    rsvp === 'awaiting' ? 'border-[#FF8D2833]' : 'border-[#3FC70A33]';
+
+  return (
+    <div className="flex flex-col gap-4 text-sm/[100%] -tracking-[0.02em] text-[#575554]">
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center justify-between">
+          <p className="font-medium text-[#222222]">{guest}</p>
+          <p className="rounded bg-[#F7F5F2] p-1">{party}</p>
+        </div>
+        <div className="flex items-center gap-1">
+          <p>{contact.phone}</p>
+          <span className="flex size-5 items-center justify-center">
+            <span className="block size-[4.38px] rounded-full bg-[#575554]"></span>
+          </span>
+          <p>{contact.email}</p>
+        </div>
+      </div>
+      <hr className="border-y-[0.5px] border-dashed border-[#00000014] bg-transparent" />
+      <div className="flex items-center justify-between [&>div]:flex [&>div]:items-center [&>div]:gap-2">
+        <div>
+          <p>Status</p>
+          <p
+            className={cn(
+              'rounded-[8px] border px-3 py-1.5 capitalize',
+              statusTextColor,
+              statusBgColor,
+              statusBorderColor,
+            )}
+          >
+            {status}
+          </p>
+        </div>
+        <div>
+          <p>RSVP</p>
+          <p
+            className={cn(
+              'rounded-[8px] border px-3 py-1.5 capitalize',
+              rsvpTextColor,
+              rsvpBgColor,
+              rsvpBorderColor,
+            )}
+          >
+            {rsvp}
+          </p>
         </div>
       </div>
     </div>
